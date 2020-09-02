b3b132bb01f02ee286be107ebc96dfcf5ed77101
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 17:07:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,17 @@ game = {:home => {:team_name => "Brooklyn Nets",
     :blocks => 1,
     :slam_dunks => 1
   },
-  
+  {
+    :player_name => "Reggie Evans",
+    :number => 0,
+    :shoe => 16,
+    :points => 22,
+    :rebounds => 12,
+    :assists => 12,
+    :steals => 3,
+    :blocks => 1,
+    :slam_dunks => 1
+  },
   ]
 },
   :away => {:team_name => "Charlotte Hornets",
