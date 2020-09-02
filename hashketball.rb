12ec37fdce8da4230358f7df1b9b4723fc01a785
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 16:49:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,8 +18,15 @@ game = {:home => {:team_name => "Brooklyn Nets",
   :away => {:team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [{
-      :player_name => "",
-      :number => 
+      :player_name => "Jeff Adrien",
+      :number => 4,
+      :shoe => 18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2 
     }]
   }
 }
