63f3ba158e31a0fd1493f7f176d26fb63c5eaa03
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 16:54:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,7 +39,17 @@ game = {:home => {:team_name => "Brooklyn Nets",
       :blocks => 12,
       :slam_dunks => 7 
     },
-    
+    {
+      :player_name => "Brook Lopez",
+      :number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 15 
+    },
     ]
   }
 }
