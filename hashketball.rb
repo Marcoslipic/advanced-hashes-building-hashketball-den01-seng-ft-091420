db7548718d86d1c69785766828f2eca85a70e934
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 16:42:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,15 @@
 # Write your code here!
 
 def game_hash
-game = {:home => {:team_name => "",
+game = {:home => {:team_name => "Brooklyn Nets",
   :colors => ["Black", "White"],
   :players => [{}]
 }
 },
-  :away => {}
+  :away => {:team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => [{}]
+  }
 }
 
 end 
